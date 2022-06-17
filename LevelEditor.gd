e8009ab4a676c07ad1b226da2d1261754ee3592f
baalmeida <bj.aa.414@gmail.com>
2022-06-16 21:59:13 -0500

    Fixed bug on ui loading data

--- LevelEditor.gd
+++ LevelEditor.gd
@@ -9,10 +9,10 @@ var util = preload("Util/Util.gd").new()
 onready var grid = $Grid 
 
 ####UI objetcs
-onready var difficult_mode = $TabContainer/items/LevelFeatures/CheckBox
-onready var bomb_prob = $TabContainer/items/LevelFeatures/BombContainer/SpinBox
-onready var line_prob = $TabContainer/items/LevelFeatures/LineRemover/SpinBox
-onready var hex_prob = $TabContainer/items/LevelFeatures/HexRemover/SpinBox
+onready var difficult_mode = $TabContainer/items/LevelFeatures/difficiltMode
+onready var bomb_prob = $TabContainer/items/LevelFeatures/BombContainer/bombPercent
+onready var line_prob = $TabContainer/items/LevelFeatures/LineRemover/linePercent
+onready var hex_prob = $TabContainer/items/LevelFeatures/HexRemover/hexPercent
 
 onready var target_goal = $TabContainer/objective/LevelGoals/MainGoal/target
 onready var target_bonus = $TabContainer/objective/LevelGoals/BonusGoal/targetBonus
@@ -37,21 +37,9 @@ onready var cells_drop_bonus = $TabContainer/objective/LevelGoals/BonusGoal/Type
 
 var button_types = []
 var buttons = []
-
+var level_edited = 0
 var levels = []
 var current_level_data = {}
-var default_level = {
-    "difficultMode": false,
-    "probBomb": 0,
-    "probLineRemover": 0,
-    "probHexRemover": 0,
-    "turns": 15,
-    "main_goal": {
-    },
-    "bonus_goal": {
-    },
-    "level_obstacles": []
-  }
 
 var main_goal =  {
       "main_goal": true,
@@ -66,8 +54,21 @@ var bonus_goal =  {
       "objective": 0,
       "cell_type": 0
     }
+
+var default_level = {
+    "difficultMode": false,
+    "probBomb": 0,
+    "probLineRemover": 0,
+    "probHexRemover": 0,
+    "turns": 15,
+    "main_goal": main_goal,
+    "bonus_goal": bonus_goal,
+    "level_obstacles": []
+  }
+
+
     
-var default_level_cells = []
+var level_cells = []
 
 ####
 # {
@@ -89,8 +90,9 @@ func _ready():
     button_types += util.SpecialCells
     button_types += util.nonClickable
     
-    levels.append(default_level)
-    current_level_data = default_level
+    current_level_data = default_level.duplicate(true) 
+    levels.append(current_level_data)
+    
     for type in button_types:
         var button = button_editor.instance()
         grid.add_child(button)
@@ -142,38 +144,31 @@ func _on_removeBonus_toggled(button_pressed):
 ####bonus radio buttons
 
 
-func _on_TotalLevels_value_changed(value):
-    current_level.max_value = value
-
-
-func _on_load_pressed():
-    current_level_label.text = str(current_level.value)
-    load_level_data_ui(levels[current_level.value])
-
 
 func load_level_data_ui(level_data):
     current_level_data = level_data
-    difficult_mode = current_level_data.difficultMode
-    bomb_prob = current_level_data.probBomb
-    line_prob = current_level_data.probLineRemover
-    hex_prob = current_level_data.probHexRemover
+    difficult_mode.toggle_mode = current_level_data.difficultMode
+    bomb_prob.value = current_level_data.probBomb
+    line_prob.value = current_level_data.probLineRemover
+    hex_prob.value = current_level_data.probHexRemover
     
-    target_goal = current_level_data.main_goal.objective
-    target_bonus = current_level_data.bonus_goal.objective
-    turns = current_level_data.turns
+    target_goal.value = current_level_data.main_goal.objective
+    target_bonus.value = current_level_data.bonus_goal.objective
+    turns.value = current_level_data.turns
     
     match (current_level_data.main_goal.goal):
         0:
             combo.pressed = true
         1:
-             points.pressed = true
+            points.pressed = true
         2:
             remove.pressed = true
+          
     match (current_level_data.bonus_goal.goal):
         0:
             combo_bonus.pressed = true
         1:
-             points_bonus.pressed = true
+            points_bonus.pressed = true
         2:
             remove_bonus.pressed = true
 
@@ -181,11 +176,6 @@ func load_level_data_ui(level_data):
         for position in obstacle.position:
             HexGrid.grid[position.x][position.y].set_type(obstacle.type)
 
-
-
-
-
- 
 
 ### Don't touch!
 func _on_difficiltMode_toggled(button_pressed):
@@ -219,6 +209,58 @@ func _on_cellDropdown_item_selected(id):
     main_goal.cell_type = cells_drop.get_item_id(id)
      
 
-
 func _on_cellDropdownBonus_item_selected(id):
     bonus_goal.cell_type = cells_drop.get_item_id(id)
+
+###level load
+
+
+func _on_TotalLevels_value_changed(value):
+    
+    var old_val = current_level.max_value
+    current_level.max_value = value - 1
+
+    var difference = (value-1) - old_val
+
+    if difference > 0:
+        for i in difference:
+            levels.append(default_level.duplicate(true))
+    else:
+        for i in (abs(difference)):
+            levels.pop_back()
+    print("---------TotalLEvel-----------")
+    print(JSON.print(levels,'\t'))
+        
+
+func _on_CurrentLevel_value_changed(value):
+    print("---------CurrentLEvel-----------")
+    print(JSON.print(levels,'\t'))
+    print("--------------------")
+    current_level_data.main_goal = main_goal
+    current_level_data.bonus_goal = bonus_goal
+    current_level_data.level_obstacles =  level_cells
+ 
+    levels[level_edited] = current_level_data
+    current_level_data =  default_level.duplicate(true)
+    level_edited = value
+    load_level_data_ui(levels[value])
+    print(JSON.print(levels,'\t'))
+
+
+
+func _on_load_pressed():
+    current_level_label.text = str(current_level.value)
+    load_level_data_ui(levels[current_level.value])
+
+
+func _on_save_pressed():
+    var file = File.new()
+    file.open("res://zzlevels.json", File.WRITE)
+    file.store_string(JSON.print(levels))
+    file.close()
+
+
+
+func _on_reset_pressed():
+    current_level_data = default_level.duplicate(true)
+    load_level_data_ui(current_level_data)
